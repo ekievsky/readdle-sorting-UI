274a1933afa3fc64ebc7b521eb0766c9eea8fe7a
Evgenii Kievsky <evgenii.kievsky@gmail.com>
2024-08-14 15:07:18 +0200

    Initial Commit

---
+++ ReaddleSortings/ReaddleSortingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReaddleSortingsApp.swift
+//  ReaddleSortings
+//
+//  Created by Yevhen Kyivskyi on 14.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct ReaddleSortingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
